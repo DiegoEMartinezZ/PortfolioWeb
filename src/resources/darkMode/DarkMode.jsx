fa8c9ce84b35e063339175904200c8a7de4606b8
DiegoEMartinezZ <diermaza.23@gmail.com>
2022-12-29 21:45:59 -0500

    Se agrego el favicon del portafolio y se cambio el nomnbre dejando atras el nombre de React APP

---
+++ src/resources/darkMode/DarkMode.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createContext } from 'react';
+
+export const DarkModeContext = createContext();
+
+export const DarkMode = () => {
+  return <div>DarkMode</div>;
+};
